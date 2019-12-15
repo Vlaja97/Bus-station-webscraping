8d81386fa1837e70a64c4f2da964f164e1940cca
Vlada vlaa <v.vlajic.vlada@gmail.com>
2019-12-15 10:34:37 -0800

    testing data sorting

--- bus_station_project/bus_station_project/spiders/bus_station_spider.py
+++ bus_station_project/bus_station_project/spiders/bus_station_spider.py
@@ -19,20 +19,17 @@ class BusStationSpider(scrapy.Spider):
         parsed_json = json.loads(response.body)
         # Split Data by PIPE ' | '
         split_by_pipe = [line.split('|') for line in parsed_json]
-        
+        #print(split_by_pipe)
 
         # Divide bus lines for each buss
         buses = [x[0] for x in split_by_pipe]
         
-       
-       
         parsed_buses_temp = [x.split(',') for x in buses]
-       
-       
-        
+        #print(parsed_buses_temp)
         # Find Longtitude and Latitude
         longtitude = [x[1] for x in split_by_pipe]
         latitude = [x[2] for x in split_by_pipe]
+        #print(latitude)
         # Convert to list of floats
         parsed_lon = [float(x) for x in longtitude]
         parsed_lat = [float(x) for x in latitude]
@@ -40,13 +37,13 @@ class BusStationSpider(scrapy.Spider):
 
         # Name of the Bus station
         name_of_bus_station = [x[3] for x in split_by_pipe]
-        
+        #print(longtitude)
        
         # Pair Data in dict
         result = defaultdict(dict)
         for name_of_bus_station, b, ln, lg in zip(name_of_bus_station, parsed_buses_temp, parsed_lon, parsed_lat):
-            result[name_of_bus_station] = {'busses': b, 'longtitude': ln, 'latitude': lg}
-       # pprint (result)
+            result[name_of_bus_station] = {'buses': b, 'longtitude': ln, 'latitude': lg}
+        pprint (result)
 
         with open('data_json.json', 'w') as outfile:
             json.dump(result, outfile, indent=4)
--- bus_station_project/map.py
+++ bus_station_project/map.py
@@ -8,21 +8,19 @@ import pandas as pd
 
 map = folium.Map(location=[45.251670,19.836940], zoom_start=14)
 address =[45.255203707,19.8416504854]
+address2 = [45.253337,19.844187]
 tooltip = 'Click For more Info'
-# Opening JSON file that we scraped and reading it as Pandas
-with open('data_json.json') as json_file:
-    data = pd.read_json(json_file)
-#print (data)
+# Opening JSON file that we scraped and reading it as Pandas data frame
+df = pd.read_json('data_json.json')
+print(type(df))
+for row in df.keys():
+  #val = df[row]
+  #print(df)
+  print(row)
+  #folium.Marker(location=[row[0],row[1]], popup='<strong>Location One</strong>', tooltip=tooltip).add_to(map)
+
+#for col in df:
+ # print(col)
 
 
-
-#folium.Marker(data[], popup='<strong>Location One</strong>', tooltip=tooltip).add_to(map)
-#for i in range(0,len(data)):
-  #  folium.Marker([data.row[i]['longtitude'], data.row[i]['latitude']], popup=data.row[i]['busses']).add_to(map)
-
-
-#folium.Marker(location=[data['busses':'latitude']]).add_to(map)
-print(data[: ,'BUL. CARA LAZARA - URBIS'])
-
-
-map.save('map.html')
+map.save('map.html')#
