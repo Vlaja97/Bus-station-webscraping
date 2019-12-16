a993c13b20ce3a6c12fe91dbdd6be037ccdc6b8a
Vlada vlaa <v.vlajic.vlada@gmail.com>
2019-12-16 10:26:28 -0800

    Formating code

--- bus_station_project/bus_station_project/spiders/bus_station_spider.py
+++ bus_station_project/bus_station_project/spiders/bus_station_spider.py
@@ -4,7 +4,6 @@ import csv
 import sys
 from pprint import pprint
 import pandas as pd
-from collections import defaultdict
 import re
 import ast
 import pickle
@@ -13,38 +12,36 @@ class BusStationSpider(scrapy.Spider):
     name = "bus_station"
     allowed_domains = ['www.gspns.rs']
     # Getting url from local bus company and scraping data for each bus
-    start_urls = ['http://www.gspns.rs/mreza-get-stajalista-tacke?linija=1']
+    start_urls = ['http://www.gspns.rs/mreza-get-stajalista-tacke?linija=16']
     lists = []
+    for url in start_urls:
+        def parse(self, response):
+            parsed_json = json.loads(response.body)
+            # Split Data by PIPE ' | '
+            split_by_pipe = [line.split('|') for line in parsed_json]
 
-    def parse(self, response):
-        parsed_json = json.loads(response.body)
-        # Split Data by PIPE ' | '
-        split_by_pipe = [line.split('|') for line in parsed_json]
+            # Divide bus lines for each buss
+            buses = [x[0] for x in split_by_pipe]
+            parsed_buses_temp = [x.split(',') for x in buses]
 
-        # Divide bus lines for each buss
-        buses = [x[0] for x in split_by_pipe]
-        parsed_buses_temp = [x.split(',') for x in buses]
-
-        # Find Longtitude and Latitude
-        longtitude = [x[1] for x in split_by_pipe]
-        latitude = [x[2] for x in split_by_pipe]
+            # Find Longtitude and Latitude
+            longtitude = [x[1] for x in split_by_pipe]
+            latitude = [x[2] for x in split_by_pipe]
+        
+            # Convert to list of floats
+            parsed_lon = [float(x) for x in longtitude]
+            parsed_lat = [float(x) for x in latitude]
+            
+            
+            # Name of the Bus station
+            name_of_bus_station = [x[3] for x in split_by_pipe]
     
-        # Convert to list of floats
-        parsed_lon = [float(x) for x in longtitude]
-        parsed_lat = [float(x) for x in latitude]
-        
-        
-        # Name of the Bus station
-        name_of_bus_station = [x[3] for x in split_by_pipe]
-        #print(longtitude)
-  
-        # Pair Data in Tuple
-        # Maybe is better to create Dictionaries!
-        result = list(zip(name_of_bus_station,parsed_buses_temp,parsed_lon,parsed_lat))
-        
-
-        with open('data.pickle', 'wb') as outfile:
-            pickle.dump(result, outfile)
+            # Pair Data in Tuple
+            # Maybe is better to create Dictionaries!
+            result = list(zip(name_of_bus_station,parsed_buses_temp,parsed_lon,parsed_lat))
+            
+            with open('data.pickle', 'wb') as outfile:
+                pickle.dump(result, outfile)
         
         
         
--- bus_station_project/map.py
+++ bus_station_project/map.py
@@ -8,18 +8,14 @@ import pandas as pd
 import pickle
 
 
-# Opening JSON file that we scraped and reading it as Pandas data frame
-#df = json.loads('data_json.json')
-
+# Opening PICKLE file that we scraped and reading it 
 with open('data.pickle', 'rb') as pickle_file:
   data = pickle.load(pickle_file)
-
-  map = folium.Map(location=[45.251670,19.836940], zoom_start=14)
-  address =45.255203707
-  address2 = 19.8416504854
+  # Creating Folium map
+  map = folium.Map(location=[45.251670,19.836940], zoom_start=15,width='75%', height='75%')
   tooltip = 'Click For more Info'
  
-
+  # Creating Folium markers for bus line
   for i in range(0,len(data)):
     folium.Marker([data[i][3], data[i][2]], popup=data[i][0], tooltip=tooltip).add_to(map)
 
